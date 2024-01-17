d31114d57e9213866ebab1545522d7f4a9e212d0
lucas-sofianos <lucas.sofianos@gmail.com>
2024-01-17 16:33:32 +0100

    modification de la partie serveur, ajout de script initialisation ufw et +

---
+++ vagServeur/script/init.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Attention l'ordre d'ajouts des règles à une importance
+
+### Initialisation de ufw (activation et affichage des règles)
+ufw status
+ufw logging on
+ufw --force enable
+ufw status
+ufw status verbose
+
+
+### Ajout de règles bloquant le traffic (entrant / sortant )
+#ufw default deny
+#ufw default deny outgoing
+
+
+
+### Ajout de règles autorisant le traffic (entrant / sortant )
+#ufw default allow
+#ufw default allow outgoing
+#http-https
+ufw allow 80
+ufw allow 443
+#ssh
+ufw allow 22 # la restreindre
+
+### Application de la configuration (rechargement de ufw).
+ufw --force reload
+ufw status verbose
+
+
+#réinitialisé ufw : sudo ufw reset --force
