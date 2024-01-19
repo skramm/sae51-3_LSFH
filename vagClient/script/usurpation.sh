972e8828f015f1d4323de2db9ee8ff6ca2863810
lucas-sofianos <lucas.sofianos@gmail.com>
2024-01-19 22:25:10 +0100

    ajout script usurpation, ajout d'un second client (autre que attaquant) pour vérification réseau interne du fonctionnement de l'usurpation. Problème fonctionnement de l'infra

--- vagClient/script/usurpation.sh
+++ vagClient/script/usurpation.sh
@@ -3,11 +3,11 @@
 # Script aux privilèges admin requis.
 
 # Récupération des informations pour usurpation :
-nmap -sSV -O -Pn -p 21,22,23,25,53,80,8080,443,110,139,143,161,162,3306,3389  192.168.1-10.1-255 # range a modifier pour être plus rapide, ou balayer plus large
+nmap -sSV -O -Pn -p 21,22,23,25,53,80,8080,443,110,139,143,161,162,3306,3389  192.168.1.1-255 # range a modifier pour être plus rapide, ou balayer plus large
 
 # Récupérer l'IP, le masque et l'adresse MAC en résultat de commande nmap.
 read -p "Entrer IP : " IP
-read -p "Entrer Masque : " Mask
+read -p "Entrer Masque (type : 24) : " Mask
 read -p "Entrer MAC address : " MAC
 
 
@@ -52,14 +52,13 @@ fi
 
 ##### fin de partie a mettre a part
 
-
-# Ajoute l'adresse IP avec le masque directement à l'interface enp0s8
-ip addr add ${IP}/${Mask} dev enp0s8
-
-
-
 # Désactive temporairement l'interface enp0s8
 ip link set dev enp0s8 down
+
+# Supprimer l'adresse IP existante
+ip addr del 192.168.1.10/24 dev enp0s8
+# Ajoute l'adresse IP avec le masque directement à l'interface enp0s8
+ip addr add ${IP}/${Mask} dev enp0s8
 
 # Modifie l'adresse MAC de l'interface enp0s8
 ip link set dev enp0s8 address ${MAC}
